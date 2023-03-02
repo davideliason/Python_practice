c525f6b5fe6b32a479ecd63d698748c6c7b54975
davideliason <davethemaker@gmail.com>
2023-03-02 06:15:46 -0800

    simple function to iterate through set of integers

---
+++ practice_files/functions.py
@@ -0,0 +1,5 @@
+def return_nums(nums):
+    for num in nums:
+        print(num)
+
+return_nums([1,2,3,4,5])
---
+++ practice_files/iterables.py
@@ -0,0 +1,33 @@
+#/ iterate dictionary
+
+dict = {"name": "Tom","name2":"Jonn"}
+"""
+# print out keys
+for title in dict:
+    print(title)
+
+# print out values
+for val in dict.values():
+    print(val)
+
+list1 = list(range(1,5))
+print("This is a list:",list1)
+print(type(list1))
+
+set1 = set(range(1,5))
+print("this is a set:",set1)
+print(type(set1))
+
+tuple1 = tuple(range(1,10))
+print("This is a tuple:",tuple1)
+print(type(tuple1))
+
+# iterate through range of numbers
+for i in range(1,20):
+    print(i)
+"""
+# iterate line by line through file
+with open('test.txt') as text:
+    words = text.read()
+
+print(words)
