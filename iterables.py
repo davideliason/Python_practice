baf7bf8cd7c2964adada77eee4d0d94ed50c0c05
davideliason <davethemaker@gmail.com>
2023-03-01 17:51:53 -0800

    iterate over dictionary to print values

--- iterables.py
+++ iterables.py
@@ -1,5 +1,12 @@
-# iterate dictionary
+#/ iterate dictionary
+
 dict = {"name": "Tom","name2":"Jonn"}
+"""
+# print out keys
 for title in dict:
     print(title)
+"""
+# print out values
+for val in dict.values():
+    print(val)
     
